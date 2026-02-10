8d534828ea31abd66833f4c7c945838578096906
reyrey112 <reydencdavies@gmail.com>
2026-02-09 22:42:02 -0500

    gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-exec uvicorn MLapp.asgi:application --host 0.0.0.0
+exec gunicorn MLapp.asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
