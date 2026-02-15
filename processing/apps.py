88f6c26af756cc86ce1ca0867c7916d08ee0ae0d
reyrey112 <reydencdavies@gmail.com>
2026-02-15 14:55:15 -0500

    testing

--- processing/apps.py
+++ processing/apps.py
@@ -30,58 +30,58 @@ class ProcessingConfig(AppConfig):
             print("Active stack doesn't exist, looking for guest stack")
             # logging.debug(f"{e}")
 
-            try:
-                stack = client.get_stack(name_id_or_prefix="guest_stack")
-                if client.active_stack.experiment_tracker is None:
-                    print("no experiment tracker found, creating new guest stack")
-                    raise KeyError
-            except KeyError as e: #Add try except statment for all of the compoenentns
-                print("no stack found, creating new guest stack")
+        try:
+            stack = client.get_stack(name_id_or_prefix="guest_stack")
+            if client.active_stack.experiment_tracker is None:
+                print("no experiment tracker found, creating new guest stack")
+                raise KeyError
+        except KeyError as e: #Add try except statment for all of the compoenentns
+            print("no stack found, creating new guest stack")
 
-                mlflow_port = os.environ.get("MLFLOW_PORT")
-                s3_bucket = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+            mlflow_port = os.environ.get("MLFLOW_PORT")
+            s3_bucket = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
-                client.create_stack_component(
-                    name="guest_tracker",
-                    flavor="mlflow",
-                    component_type=StackComponentType.EXPERIMENT_TRACKER,
-                    configuration={
-                        "tracking_uri": f"http://127.0.0.1:{mlflow_port}",
-                        "tracking_username": "guest",
-                        "tracking_password": "Guest1.",
-                    },
-                )
-                
-                client.create_stack_component(
-                    name="guest_model_registry",
-                    flavor="mlflow",
-                    component_type=StackComponentType.MODEL_REGISTRY,
-                    configuration={},
-                )
+            client.create_stack_component(
+                name="guest_tracker",
+                flavor="mlflow",
+                component_type=StackComponentType.EXPERIMENT_TRACKER,
+                configuration={
+                    "tracking_uri": f"http://127.0.0.1:{mlflow_port}",
+                    "tracking_username": "guest",
+                    "tracking_password": "Guest1.",
+                },
+            )
+            
+            client.create_stack_component(
+                name="guest_model_registry",
+                flavor="mlflow",
+                component_type=StackComponentType.MODEL_REGISTRY,
+                configuration={},
+            )
 
-                client.create_stack_component(
-                    name="guest_deployer",
-                    flavor="docker",
-                    component_type=StackComponentType.DEPLOYER,
-                    configuration={},
-                )
+            client.create_stack_component(
+                name="guest_deployer",
+                flavor="docker",
+                component_type=StackComponentType.DEPLOYER,
+                configuration={},
+            )
 
-                client.create_stack_component(
-                    name="guest_artifact_store",
-                    flavor="s3",
-                    component_type=StackComponentType.ARTIFACT_STORE,
-                    configuration={"path": f"s3://{s3_bucket}"},
-                )
+            client.create_stack_component(
+                name="guest_artifact_store",
+                flavor="s3",
+                component_type=StackComponentType.ARTIFACT_STORE,
+                configuration={"path": f"s3://{s3_bucket}"},
+            )
 
-                client.create_stack(
-                    name="guest_stack",
-                    components={
-                        StackComponentType.ORCHESTRATOR: "default",
-                        StackComponentType.ARTIFACT_STORE: "guest_artifact_store",
-                        StackComponentType.EXPERIMENT_TRACKER: "guest_tracker",
-                        StackComponentType.MODEL_REGISTRY: "guest_model_registry",
-                        StackComponentType.DEPLOYER: "guest_deployer",
-                    },
-                )
+            client.create_stack(
+                name="guest_stack",
+                components={
+                    StackComponentType.ORCHESTRATOR: "default",
+                    StackComponentType.ARTIFACT_STORE: "guest_artifact_store",
+                    StackComponentType.EXPERIMENT_TRACKER: "guest_tracker",
+                    StackComponentType.MODEL_REGISTRY: "guest_model_registry",
+                    StackComponentType.DEPLOYER: "guest_deployer",
+                },
+            )
 
-                client.activate_stack("guest_stack")
+            client.activate_stack("guest_stack")
