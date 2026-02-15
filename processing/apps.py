41e285938198d7fb2f0db928dc875623d39b6062
reyrey112 <reydencdavies@gmail.com>
2026-02-15 15:42:41 -0500

    testing

--- processing/apps.py
+++ processing/apps.py
@@ -15,7 +15,7 @@ class ProcessingConfig(AppConfig):
 
         from zenml.client import Client
         from zenml.enums import StackComponentType
-        from zenml.exceptions import CredentialsNotValid
+        from zenml.exceptions import CredentialsNotValid, EntityExistsError
 
         print("initializing zenml client")
         client = Client()
@@ -40,48 +40,63 @@ class ProcessingConfig(AppConfig):
 
         mlflow_port = os.environ.get("MLFLOW_PORT")
         s3_bucket = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+        try:
+            client.create_stack_component(
+                name="guest_tracker",
+                flavor="mlflow",
+                component_type=StackComponentType.EXPERIMENT_TRACKER,
+                configuration={
+                    "tracking_uri": f"http://127.0.0.1:{mlflow_port}",
+                    "tracking_username": "guest",
+                    "tracking_password": "Guest1.",
+                },
+            )
 
-        client.create_stack_component(
-            name="guest_tracker",
-            flavor="mlflow",
-            component_type=StackComponentType.EXPERIMENT_TRACKER,
-            configuration={
-                "tracking_uri": f"http://127.0.0.1:{mlflow_port}",
-                "tracking_username": "guest",
-                "tracking_password": "Guest1.",
-            },
-        )
-        
-        client.create_stack_component(
-            name="guest_model_registry",
-            flavor="mlflow",
-            component_type=StackComponentType.MODEL_REGISTRY,
-            configuration={},
-        )
+        except EntityExistsError as e:
+            print("experiment tracker exists, continuing")
 
-        client.create_stack_component(
-            name="guest_deployer",
-            flavor="docker",
-            component_type=StackComponentType.DEPLOYER,
-            configuration={},
-        )
+        try:
+            client.create_stack_component(
+                name="guest_model_registry",
+                flavor="mlflow",
+                component_type=StackComponentType.MODEL_REGISTRY,
+                configuration={},
+            )
+        except EntityExistsError as e:
+            print("model registry exists, continuing")
 
-        client.create_stack_component(
-            name="guest_artifact_store",
-            flavor="s3",
-            component_type=StackComponentType.ARTIFACT_STORE,
-            configuration={"path": f"s3://{s3_bucket}"},
-        )
+        try:
+            client.create_stack_component(
+                name="guest_deployer",
+                flavor="docker",
+                component_type=StackComponentType.DEPLOYER,
+                configuration={},
+            )
+        except EntityExistsError as e:
+            print("experiment tracker exists, continuing")
+            
+        try:
+            client.create_stack_component(
+                name="guest_artifact_store",
+                flavor="s3",
+                component_type=StackComponentType.ARTIFACT_STORE,
+                configuration={"path": f"s3://{s3_bucket}"},
+            )
+        except EntityExistsError as e:
+            print("artifact store exists, continuing")
 
-        client.create_stack(
-            name="guest_stack",
-            components={
-                StackComponentType.ORCHESTRATOR: "default",
-                StackComponentType.ARTIFACT_STORE: "guest_artifact_store",
-                StackComponentType.EXPERIMENT_TRACKER: "guest_tracker",
-                StackComponentType.MODEL_REGISTRY: "guest_model_registry",
-                StackComponentType.DEPLOYER: "guest_deployer",
-            },
-        )
+        try:
+            client.create_stack(
+                name="guest_stack",
+                components={
+                    StackComponentType.ORCHESTRATOR: "default",
+                    StackComponentType.ARTIFACT_STORE: "guest_artifact_store",
+                    StackComponentType.EXPERIMENT_TRACKER: "guest_tracker",
+                    StackComponentType.MODEL_REGISTRY: "guest_model_registry",
+                    StackComponentType.DEPLOYER: "guest_deployer",
+                },
+            )
+        except EntityExistsError as e:
+            print("stack exists, continuing")
 
         client.activate_stack("guest_stack")
