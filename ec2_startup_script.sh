4ab5806e00e688cf8d78fe1e95c58f6dfa451bb6
reyrey112 <reydencdavies@gmail.com>
2026-02-16 13:50:22 -0500

    update for ssl and my domain

--- ec2_startup_script.sh
+++ ec2_startup_script.sh
@@ -18,6 +18,7 @@ sudo apt install software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
 sudo apt install python3.13 python3.13-venv python3.13-dev
+sudo apt install -y certbot python3-certbot-nginx
 
 if [ ! -d "venv_MLAPP" ]; then
     python3.13 -m venv venv_MLAPP
@@ -39,25 +40,25 @@ python3.13 download_from_ssm.py
 sudo apt update
 sudo apt install nginx
 
-TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
-    -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+# TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
+#     -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
-echo "SERVER_NAME=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" \
-  http://169.254.169.254/latest/meta-data/public-ipv4)" | sudo tee -a /home/ubuntu/MLAPP/.env
+# echo "SERVER_NAME=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" \
+#   http://169.254.169.254/latest/meta-data/public-ipv4)" | sudo tee -a /home/ubuntu/MLAPP/.env
 
 set -a
 source /home/ubuntu/MLAPP/.env
 set +a
 
 
-if [ ! -f "nginx-selfsigned.crt" ] || [ ! -f "nginx-selfsigned.key" ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout nginx-selfsigned.key -out nginx-selfsigned.crt \
-        -subj "/C=US/ST=New Jersey/L=East Brunswick/O=Insight4Data/OU=ENG/CN=${SERVER_NAME}"
-fi
+# if [ ! -f "nginx-selfsigned.crt" ] || [ ! -f "nginx-selfsigned.key" ]; then
+#     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#         -keyout nginx-selfsigned.key -out nginx-selfsigned.crt \
+#         -subj "/C=US/ST=New Jersey/L=East Brunswick/O=Insight4Data/OU=ENG/CN=${SERVER_NAME}"
+# fi
 
-export SSL_CERTIFICATE=/home/ubuntu/MLAPP/nginx-selfsigned.crt
-export SSL_CERTIFICATE_KEY=/home/ubuntu/MLAPP/nginx-selfsigned.key
+export SSL_CERTIFICATE=/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem
+export SSL_CERTIFICATE_KEY=/etc/letsencrypt/live/${SERVER_NAME}/privkey.pem
 
 
 envsubst '${DJANGO_PORT} ${MLFLOW_PORT} ${ZENML_PORT} ${SERVER_NAME} ${SSL_CERTIFICATE} ${SSL_CERTIFICATE_KEY}' \
