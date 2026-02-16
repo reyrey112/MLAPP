6342cd6ae58c585cd497ac2461129982d0dca753
reyrey112 <reydencdavies@gmail.com>
2026-02-16 12:18:08 -0500

    testing s3 varibale

--- MLapp/settings.py
+++ MLapp/settings.py
@@ -46,8 +46,9 @@ else:
     }
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
+print("getting bucket NAME")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+print(f"printing buket name {os.environ.get("AWS_STORAGE_BUCKET_NAME")}")
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_REGION_NAME = "us-east-2"
 
