f09c26cd7c44bbc19b8b5bed6d86d6a919e7bef3
reyrey112 <reydencdavies@gmail.com>
2026-02-23 14:14:46 -0500

    testing tracking uri fix

--- MLOps/steps/zen_log_model.py
+++ MLOps/steps/zen_log_model.py
@@ -2,11 +2,19 @@ import pickle
 from zenml import step
 from zenml.client import Client
 import mlflow
+from zenml.integrations.mlflow.experiment_trackers.mlflow_experiment_tracker import (
+    MLFlowExperimentTracker,
+)
+
 client = Client()
+experiment_tracker: MLFlowExperimentTracker
 
 experiment_tracker = client.active_stack.experiment_tracker
+
+
 @step(experiment_tracker=experiment_tracker.name)
 def log_model(model_path: str, run_name: str):
+    mlflow.set_tracking_uri(experiment_tracker.get_tracking_uri)
     with open(model_path, "rb") as f:
         model = pickle.load(f)
     mlflow.sklearn.log_model(model, name=run_name)
