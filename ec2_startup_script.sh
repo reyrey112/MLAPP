ef8600fd592e73652a1bae120ccccf39d4ba2fc3
reyrey112 <reydencdavies@gmail.com>
2026-02-23 15:53:57 -0500

    test

--- ec2_startup_script.sh
+++ ec2_startup_script.sh
@@ -59,4 +59,4 @@ sudo aws ecr get-login-password --region us-east-2 | sudo docker login --usernam
 
 sudo docker-compose pull
 sudo docker image prune --force
-sudo docker-compose -f docker-compose.yaml up -d
+sudo docker-compose -f docker-compose.yaml up --env-file .env -d
