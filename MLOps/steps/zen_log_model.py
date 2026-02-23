043b7ebc102f83756606e1afc699e79115db3907
reyrey112 <reydencdavies@gmail.com>
2026-02-23 14:24:52 -0500

    uri fix testing

--- MLOps/steps/zen_log_model.py
+++ MLOps/steps/zen_log_model.py
@@ -14,7 +14,9 @@ experiment_tracker = client.active_stack.experiment_tracker
 
 @step(experiment_tracker=experiment_tracker.name)
 def log_model(model_path: str, run_name: str):
-    mlflow.set_tracking_uri(experiment_tracker.get_tracking_uri)
+    uri = experiment_tracker.get_tracking_uri()
+    print(f"{uri}")
+    mlflow.set_tracking_uri(uri)
     with open(model_path, "rb") as f:
         model = pickle.load(f)
     mlflow.sklearn.log_model(model, name=run_name)
